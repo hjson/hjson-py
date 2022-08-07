6fb7fabedf4f7eb86729ac68eea98b84151ccd09
Greg Roodt <greg@canva.com>
2022-08-07 20:37:14 +1000

    Use entrypoints instead of scripts

--- setup.py
+++ setup.py
@@ -72,7 +72,12 @@ def run_setup():
         license="MIT License",
         packages=['hjson', 'hjson.tests'],
         platforms=['any'],
-        scripts=['bin/hjson', 'bin/hjson.cmd',],
-        **kw)
+        entry_points={
+            "console_scripts": [
+                "hjson = hjson.tool:main",
+            ],
+        },
+        **kw,
+    )
 
 run_setup()
